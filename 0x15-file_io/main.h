b8c076f89c08f4e2740a76542040ee955410150c
aoubid <aoubid.tm@gmail.com>
2023-10-30 16:56:08 -0700

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: holberton.h
+ * Auth: Amine OUBID
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
 
 #include <sys/types.h>
 #include <sys/stat.h>
